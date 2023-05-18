f8eb0a30902eb14808059b6eabea1273983ac568
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-05-18 17:13:49 +0700

    start

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRFEYlAtoACnTpswgXq5DajkuqemVtJR0",
+  authDomain: "resto-ae0b5.firebaseapp.com",
+  projectId: "resto-ae0b5",
+  storageBucket: "resto-ae0b5.appspot.com",
+  messagingSenderId: "671527093331",
+  appId: "1:671527093331:web:5c457adda2e4cd06a6b2a6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
