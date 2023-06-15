7d68118823e576b33e83b5825928b16bb662532d
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-06-15 23:47:29 +0400

    test

--- index.js
+++ index.js
@@ -139,7 +139,10 @@ const formattedDate = `${dayOfWeek}, ${day} ${month} ${year}`;
 dateToday.textContent = formattedDate;
 
 //search
+let dishNotFoundMessage = null;
+
 searchInput.addEventListener('input', handleSearch);
+
 searchInput.addEventListener('keydown', (event) => {
     if (event.key === 'Backspace' || event.key === 'Delete') { // delete message when deleting letters from search input
         if (searchInput.value === '') {
@@ -147,7 +150,6 @@ searchInput.addEventListener('keydown', (event) => {
         }
     }
 });
-let dishNotFoundMessage = null;
 
 function handleSearch() {
     const searchText = searchInput.value.toLowerCase();
@@ -157,7 +159,8 @@ function handleSearch() {
 
     dishes.forEach((dish) => {
         const dishType = dish.dataset.type;
-        const dishTitle = dish.querySelector('.main-content__offers-dish-title').textContent.toLowerCase();
+        const dishTitleElement = dish.querySelector('.main-content__offers-dish-title');
+        const dishTitle = dishTitleElement ? dishTitleElement.textContent.toLowerCase() : '';
 
         if ((activeTab.textContent.toLowerCase() === 'all' || dishType === activeTab.textContent.toLowerCase()) && dishTitle.includes(searchText)) {
             dish.style.display = '';
@@ -184,8 +187,10 @@ function handleSearch() {
 
 function clearSearchInput() {
     searchInput.value = '';
+    handleSearch();
     if (dishNotFoundMessage) {
         dishNotFoundMessage.remove();
+        dishNotFoundMessage = null;
     }
 }
 //Orders
